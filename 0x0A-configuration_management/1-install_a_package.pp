848b049e64e55810f4295424a33fcf4f7527182a
BOULAKNADEL <boulaknadelhamza@gmail.com>
2024-06-29 01:18:59 +0100

    fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,5 @@
 # Install puppet-lint
-
-exec { 'install_flask':
-  command => 'pip3 install flask==2.1.0',
-  path    => ['/bin', '/usr/bin'],
-  unless  => 'pip3 show flask | grep -q "Version: 2.1.0"',
+package { 'puppet-lint':
+  ensure   => '2.5.0',
+  provider => 'gem'
 }
